c3d6fe2c62a1d10f849b33a7de960076cf839c63
Nikita Tchayka <nikitatchayka@gmail.com>
2017-08-02 13:02:26 +0100

    WIP: CSV Combiner and filler

--- data-combinator/src/DataCombinator/Csv.hs
+++ data-combinator/src/DataCombinator/Csv.hs
@@ -15,17 +15,33 @@ import Analyze.RFrame (RFrame)
 import Data.Text (Text)
 import Data.Vector (Vector)
 
-combine :: (MonadThrow m) => NonEmpty [RFrame Text Text] -> m (RFrame Text Text)
-combine frames = do
-    let baseFrame = head frames
-                  & Analyze.keepCols isTimeSeriesColumn
-    let columns = getNonEmpty frames
-                & fmap ( Analyze.dropCols (`isColumnOf` baseFrame) )
-    foldM Analyze.extendCols baseFrame columns
-  where
-    isTimeSeriesColumn k = k `elem` [ "epoc (ms)", "timestamp (-05:00)", "elapsed (s)" ]
+combine :: NonEmpty [RFrame Text Text] -> IO (RFrame Text Text)
+combine frames
+  = foldM extend (head frames) (tail frames)
+  & fmap (f "elapsed (s)")
+ where
+  f = error "Sort not implemented"
 
+
+
+-- TODO: MAKE A PR TO ANALYZE
+
+-- | Checks that a key is contained in the 'RFrame'
 isColumnOf :: (Analyze.Data k) => k -> RFrame k v -> Bool
 isColumnOf key frame = Analyze.keepCols (== key) frame
                      & Analyze.numCols
                      & (/= 0)
+
+
+-- | Extends an RFrame with another, adding empty values to missing columns
+extend :: RFrame Text Text -> RFrame Text Text -> IO (RFrame Text Text)
+extend a b = do
+  let aKeys = Analyze.rframeKeys a
+  let bKeys = Analyze.rframeKeys b
+  let aNewKeys = Vector.filter (not . (`Vector.elem` aKeys)) (Analyze.rframeKeys a)
+  let bNewKeys = Vector.filter (not . (`Vector.elem` bKeys)) (Analyze.rframeKeys b)
+  extendedA <- foldM newEmptyColumn a bNewKeys
+  extendedB <- foldM newEmptyColumn b aNewKeys
+  Analyze.appendRows extendedA extendedB
+ where
+  newEmptyColumn frame newKey = Analyze.addColumn frame newKey (Vector.replicate (Analyze.numRows frame) "")
