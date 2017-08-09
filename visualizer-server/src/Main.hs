0b651c1ffe978a9d5a38aaf85208f34ccdda105d
Nikita Tchayka <nikitatchayka@gmail.com>
2017-08-09 12:36:52 +0100

    Add miso client, move html to server

--- visualizer-server/src/Main.hs
+++ visualizer-server/src/Main.hs
@@ -6,33 +6,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
+
 module Main where
 
-import Debug.Trace
-import GHC.Generics
-import Servant
-import Servant.API
-import Network.Wai
-import Network.Wai.Middleware.Cors
-import Network.Wai.Handler.Warp
-import Data.Aeson
 import qualified Analyze
+import Control.Monad.IO.Class
+import Data.Aeson
+import Data.Function ((&))
+import Data.Text (pack, unpack)
 import qualified Data.Vector as Vector
 import Data.Vector (Vector)
-import Data.Text (pack, unpack)
-import Control.Monad.IO.Class
-import Data.Function ((&))
-
+import Debug.Trace
+import GHC.Generics
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
+import Servant
+import Servant.API
 
 downsampleFactor :: Int
-downsampleFactor = 500
+downsampleFactor = 5000
 
 type DataAPI
-  = "data"
-  :> Capture "header" String
-  :> Capture "start" Double
-  :> Capture "end" Double
-  :> Get '[JSON] (Vector DataSeries)
+     = "data"
+     :> Capture "header" String
+     :> Capture "start" Double
+     :> Capture "end" Double
+     :> Get '[ JSON] (Vector DataSeries)
+     :<|> Raw
 
 type DataPoint = [Double]
 
@@ -46,50 +47,57 @@ instance ToJSON DataSeries
 dataAPI :: Proxy DataAPI
 dataAPI = Proxy
 
-
 dataServer :: Server DataAPI
 dataServer = dataHandler
+    :<|> serveDirectoryFileServer "static"
 
 downsample :: Int -> DataSeries -> DataSeries
-downsample threshold (DataSeries nm v) = DataSeries nm
-    ( v
-    & Vector.iterateN vLength (Vector.drop n)
-    & Vector.takeWhile vectorNotNull
-    & Vector.map Vector.head
-    )
+downsample threshold (DataSeries nm v) =
+    DataSeries
+        nm
+        ( v
+        & Vector.iterateN vLength (Vector.drop n)
+        & Vector.takeWhile vectorNotNull
+        & Vector.map Vector.head
+        )
   where
     vLength = Vector.length v
     vectorNotNull = not . Vector.null
     n = vLength `div` threshold
 
-
 dataHandler :: String -> Double -> Double -> Handler (Vector DataSeries)
 dataHandler header start end = do
-    df <- liftIO $ Analyze.loadCSVFileWithHeader $ "../data/" ++ header ++ "combined.csv"
+    df <- liftIO $
+        Analyze.loadCSVFileWithHeader $ "../data/" ++ header ++ "combined.csv"
     let dfKeys = Vector.take 9 $ Vector.drop 4 $ Analyze.rframeKeys df
     varElapsed <- liftIO $ Analyze.col "elapsed" df
     let doubleElapsed = Vector.map (read . unpack) varElapsed :: Vector Double
     series <- liftIO $ Vector.mapM (serieColumn df doubleElapsed) dfKeys
-    return $ Vector.map (downsample downsampleFactor) $ getWindow start end series
+    return $ series
+           & getWindow start end
+           & Vector.map (downsample downsampleFactor)
   where
-      getWindow start end dataSeriesVector
-        = Vector.map (adjustDataSeries start end) dataSeriesVector
-      adjustDataSeries start end (DataSeries n dps) = DataSeries n
-          ( dps
-          & Vector.takeWhile (\(elapsedSeconds:_) -> elapsedSeconds < end)
-          & Vector.dropWhile (\(elapsedSeconds:_) -> elapsedSeconds < start)
-          )
-      serieColumn df doubleElapsed colName = do
+    getWindow start end dataSeriesVector =
+        Vector.map (adjustDataSeries start end) dataSeriesVector
+    adjustDataSeries start end (DataSeries n dps) =
+        DataSeries
+            n
+            ( dps
+            & Vector.takeWhile (\(elapsedSeconds:_) -> elapsedSeconds < end)
+            & Vector.dropWhile (\(elapsedSeconds:_) -> elapsedSeconds < start)
+            )
+    serieColumn df doubleElapsed colName = do
         varCol <- Analyze.col colName df
-        let doubleVariable = (Vector.map (read . unpack) varCol :: Vector Double)
-        return $ DataSeries
-            (unpack colName)
-            (Vector.zipWith (\a b -> [a,b]) doubleElapsed doubleVariable)
+        let doubleVariable =
+                (Vector.map (read . unpack) varCol :: Vector Double)
+        return $
+            DataSeries
+                (unpack colName)
+                (Vector.zipWith (\a b -> [a, b]) doubleElapsed doubleVariable)
 
 main :: IO ()
 main = do
     putStrLn "Running server"
-    run 8081 $ do
-        simpleCors app
+    run 8081 app
   where
     app = serve dataAPI dataServer
