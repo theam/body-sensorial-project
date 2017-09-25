a23666bc592930493a114fb5ad648dde1bd15fe0
Nikita Tchayka <nikitatchayka@gmail.com>
2017-09-25 14:20:07 +0100

    WIP

--- segmentation/src/Main.hs
+++ segmentation/src/Main.hs
@@ -6,14 +6,21 @@ import Inliterate.Import
 This module comprises the segmentation part of the preprocessing of the data, applying whats
 described in the paper
 
-> **Robust heart sound segmentation algorithm for commonly occurring heart valve diseases**
-> *Ari et al, 2008*
+> **Automatic segmentation of Phonocardiogram using the occurrence of the cardiac events**
+> *Vishwanath et al., 2017*
 
-## Process
+## 1. Process
 
 The process that is described in the paper can be mapped to the following commutative diagram:
 
-![commdiag](http://i.imgur.com/PSjAwGO.png)
+$$\require{AMScd}$$
+
+$$\begin{CD}
+Noisy Sound @>prefilter>> Filtered Sound @>stft>> Spectrogram\\\\
+@. @. @VV{barkscale}V\\\\
+Event Detection Function @<loudnessEvaluation<< SmoothSpectrogram @<smoothen<< BarkScaled Spectrogram
+\end{CD}$$
+
 
 The [classes](https://en.wikipedia.org/wiki/Class_(set_theory))
 in this commutative diagram can be represented directly as data types in Haskell:
@@ -31,8 +38,8 @@ These are [Tags](https://hackage.haskell.org/package/tagged-0.8.5/docs/Data-Tagg
 that will be used for the definition of our format underneath. They help us not being
 constrained to any kind of format (`Vector`, `Array`, `ByteString`, etc...).
 
-The [morphisms](https://en.wikipedia.org/wiki/Morphism) represented in the diagram would
-transform the data in some way:
+The [morphisms](https://en.wikipedia.org/wiki/Morphism) represented in the diagram are functions
+that would transform the data in some way:
 
 1. `prefilter` would apply a [Chebyshev type I Lowpass filter](https://en.wikipedia.org/wiki/Chebyshev_filter)
 2. An `stft` would be applied to all the `FilteredSound` using a *3ms* [Hann Window](https://en.wikipedia.org/wiki/Window_function#Cosine-sum_windows)
@@ -40,7 +47,17 @@ transform the data in some way:
 4. `smoothen` convolves each of the frequency bands using a *200ms* Hamming window (raised cosine).
 5. `loudnessEvaluation` sums all the amplitudes of all frequency bands:
 
-![eq](http://i.imgur.com/l4h6Hr8.png)
+	$$ L_{dB}(t) = \frac{\sum_{k=1}{N}E_k}{N} $$
 
-where *Ek* represents the magnitude of the kth frequency band in the spectrogram.
-There are *N* of them.
+	where \\(E_k\\) represents the magnitude of the kth frequency band in the spectrogram.
+	There are \\(N\\) of them.
+6. Now we convolute the loudness with a *300ms* Hamming window, so we obtain a smoothed loudness
+	index function, where positive peaks are **onsets**, and negative ones are **offsets**
+    
+After doing this process, we can locate manually the \\(S_1\\) and \\(S_2\\) sounds, which help us
+to locate the *systole* and *diastole*. After that, it's just a matter of alternation. So, we
+can automate the segmentation of the next sounds.
+
+## 2. Implementation
+
+We will be 
